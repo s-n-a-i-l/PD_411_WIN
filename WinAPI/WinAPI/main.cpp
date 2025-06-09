9bc682c8ae1c89c04866a0ceed06a00a51c19764
User <User@DESKTOP-QHG18FL>
2025-06-09 15:40:59 +0300

    Dialog start

---
+++ WinAPI/WinAPI/main.cpp
@@ -0,0 +1,42 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//#define MESSAGE_BOX
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+
+#ifdef MESSAGE_BOX
+	MessageBox
+	(
+		NULL, //
+		"Привет WinAPI!\nЭто самое простое окно - окно сообщения\t\t\t\t(MessageBox)",
+		"Привет!",
+		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION //| MB_HELP
+		| MB_DEFBUTTON4
+		| MB_TOPMOST
+	);
+	//MB_ - MessageBox
+	//Венгерска нотация  
+#endif // MESSAGE_BOX
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		break;
+	}
+	return FALSE;
+}
